c2c5cd06c58ae64397d735155345faf31f9b48af
Diane Looney <diane@dianelooney.com>
2020-02-09 00:07:15 -0500

    Add colorize fn to hydra

--- src/hydra/core.clj
+++ src/hydra/core.clj
@@ -1,14 +1,11 @@
 (ns hydra.core
   (:gen-class))
 
-
-(def prelude (atom ""))
-
 (defn mix-args [args base]
   (concat (take (count base) args)
           (nthrest base (count args))))
 
-(defn colorizer [name base]
-  (fn [pipe & args]
-    {:func name
-     :args (mix-args args base)}))
+(defn colorize [name base glsl args]
+  [{:name name
+    :decl glsl
+    :args (mix-args args base)}])
--- test/hydra/core_test.clj
+++ test/hydra/core_test.clj
@@ -8,3 +8,19 @@
     []      [:a] []
     [:a :b] []   [:a :b]
     [:c :b] [:c] [:a :b]))
+
+(deftest test-colorize
+  (is (= (colorize "bananas" [:a :b] "glsl" [])
+         [{:name "bananas"
+           :decl "glsl"
+           :args '(:a :b)}])))
+
+(deftest test-partial-colorize
+  (let [p (partial colorize "bananas" [:a :b] "glsl")]
+    (are [x y] (= x y)
+      (p []) [{:name "bananas"
+            :decl "glsl"
+            :args '(:a :b)}]
+      (p [:c]) [{:name "bananas"
+               :decl "glsl"
+               :args '(:c :b)}])))
